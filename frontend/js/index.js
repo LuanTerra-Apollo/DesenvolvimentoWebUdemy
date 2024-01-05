009d5a01d936fef08f6f558430e3d2a5ad2a221a
LuanTerra-Apollo <luan_terra12@hotmail.com>
2024-01-05 14:45:35 -0300

    feat: Comunicação Front => Back, página de produtos, cadastrando e listando.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,47 @@
 var tbody = document.querySelector("table>tbody");
+var form = {
+    nome: document.querySelector('#nome'),
+    quantidade: document.querySelector('#quantidade'),
+    valor: document.querySelector('#valor'),
+    btnSalvar: document.querySelector('#btn-salvar'),
+    btnCancelar: document.querySelector('#btn-cancelar'),
+}
+
+form.btnSalvar.addEventListener('click', () => {
+    var produto = {
+        nome: form.nome.value,
+        quantidadeEstoque: form.quantidade.value,
+        valor: form.valor.value
+    };
+    
+    // Aqui preciso verificar se os campos foram preenchidos
+    if(!produto.nome || !produto.quantidadeEstoque || !produto.valor) {
+        // Se não foram, mandar mensagem para o usuário preencher.
+        alert("Os campos nome, quantidade e valor são obrigatórios!");
+        return;
+    }
+    // Caso contrário enviar os dados para salvar no backend.
+    cadastrarProdutoNaAPI(produto)
+})
+
+function cadastrarProdutoNaAPI(produto){
+    fetch('http://localhost:3000/produtos',{
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method:"POST",
+        body: JSON.stringify(produto),
+    }) //endereço da
+    .then(response => response.json())
+    .then( response => {
+        obterProdutosDaAPI();
+        limparCampos();
+    } )
+    .catch(erro => {
+        console.log(erro)
+        alert("Deu ruim")
+    })
+}
 
 function obterProdutosDaAPI() {
     fetch('http://localhost:3000/produtos') //endereço da
@@ -11,6 +54,9 @@ function obterProdutosDaAPI() {
 }
 
 function preencherTabela(produtos) {
+
+    //Limpando a tabela para receber os produtos.
+    tbody.textContent = "";
 
     produtos.map(produto => {
         var tr = document.createElement("tr");
@@ -32,6 +78,12 @@ function preencherTabela(produtos) {
         tbody.appendChild(tr);
     })
 
+}
+
+function limparCampos(){
+    form.nome.value = "";
+    form.quantidade.value = "";
+    form.valor.value = "";
 }
 
 obterProdutosDaAPI();
